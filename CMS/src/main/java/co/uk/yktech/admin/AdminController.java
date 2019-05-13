2a7a5d70196abb4b62626786fd9e27f4d725affe
yakampe <yanis.kampe.cv@gmail.com>
2019-05-13 19:28:45 +0100

    #admin module
    1.admin navbar dropdwn menu;
    2.admin dashboard of pages;

--- CMS/src/main/java/co/uk/yktech/admin/AdminController.java
+++ CMS/src/main/java/co/uk/yktech/admin/AdminController.java
@@ -1,16 +1,31 @@
 package co.uk.yktech.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.uk.yktech.page.CMSPage;
+import co.uk.yktech.page.CMSPageRepository;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	CMSPageRepository CMSPageRepo;
+	
 	@GetMapping("/home")
 	public String home() {
 		return "admin-homepage";
+	}
+	
+	@GetMapping("/pages")
+	public String showPages(Model theModel) {
+		Iterable<CMSPage> pages = CMSPageRepo.findAll();
+		theModel.addAttribute("pages", pages);
+		return "admin/pages";
 	}
 	
 }
