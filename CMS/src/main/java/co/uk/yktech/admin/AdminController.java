cc9ae02a1aaf23daea89fc21f828d1421ac5380a
yakampe <yanis.kampe.cv@gmail.com>
2019-05-13 20:49:50 +0100

    #basic page editing
    added basic page editing

--- CMS/src/main/java/co/uk/yktech/admin/AdminController.java
+++ CMS/src/main/java/co/uk/yktech/admin/AdminController.java
@@ -1,9 +1,14 @@
 package co.uk.yktech.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.uk.yktech.page.CMSPage;
@@ -13,6 +18,8 @@ import co.uk.yktech.page.CMSPageRepository;
 @RequestMapping("/admin")
 public class AdminController {
 
+	Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
 	@Autowired
 	CMSPageRepository CMSPageRepo;
 	
@@ -26,6 +33,22 @@ public class AdminController {
 		Iterable<CMSPage> pages = CMSPageRepo.findAll();
 		theModel.addAttribute("pages", pages);
 		return "admin/pages";
+	}
+	@GetMapping("/editPage/{pageTitle}")
+	public String editPage(@PathVariable String pageTitle, Model theModel) {
+		logger.info("Loading page for editing with title == " +pageTitle);
+		try {
+		CMSPage page = CMSPageRepo.findByPageTitle(pageTitle).get(0);
+		theModel.addAttribute("page", page);
+		} catch(IndexOutOfBoundsException e) {
+		logger.warn("Page Not Found");
+		}
+		return "admin/editpage";
+	}
+	@PostMapping("/savePage")
+	public String savePage(@ModelAttribute("page") CMSPage page) {
+		CMSPageRepo.save(page);
+		return "admin-homepage";
 	}
 	
 }
