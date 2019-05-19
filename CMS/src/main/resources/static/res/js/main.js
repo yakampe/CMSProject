4ff38414bee32ca16b916d06871b27a1f92ace9f
yakampe <yanis.kampe.cv@gmail.com>
2019-05-19 16:58:12 +0100

    #mobile sidebar
    sidebar is a menu if not on desktop

--- CMS/src/main/resources/static/res/js/main.js
+++ CMS/src/main/resources/static/res/js/main.js
@@ -1,6 +1,16 @@
 //menu button
 $(function() {
+	// phone functionality for blog
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+			.test(navigator.userAgent)) {
+		$("#sidebar").hide();
+		$("#sidebarbutton").show();
+	}
+	$("#sidebarbutton").click(function() {
+		$("#sidebar").toggle("slow");
+	});
 
+	// layered menu
 	var bodyOverflow = $("body").css("overflow");
 	$('#menuButton').click(function() {
 		if ($("body").css("overflow") == "hidden") {
@@ -14,50 +24,13 @@ $(function() {
 		}
 
 	});
-});
 
-function menuLinks(open) {
-	if (open) {
-		$(".menuLink").first().show(900, function showNext() {
-			$(this).next(".menuLink").show("fast", showNext);
-			console.log("doing");
-		});
-		$(".menuhr").each(
-				function(index) {
-					console.log(index
-							+ " : "
-							+ $(this).css("width",
-									((100 / $(".menuhr").length) * ($(".menuhr").length -index))+"%"));
-				});
-	} else {
-		$(".menuLink").first().hide(200, function showNext() {
-			$(this).next(".menuLink").hide("fast", showNext);
-			console.log("doing");
-		});
-
-	}
-}
-
-// show navbar and stop video in homepage
-function scrollFunction() {
-
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById('bgVideo').pause();
-		$("#navbar-wrap").fadeIn(300);
-	} else {
-		$("#navbar-wrap").fadeOut(300);
-		document.getElementById('bgVideo').play();
-	}
-}
-
-// Scrolling sidebar in blog
-$(function() {
-
+	// blog sidebar
 	var $sidebar = $("#sidebar"), $window = $(window), offset = $sidebar
 			.offset(), topPadding = 150;
 
 	$window.scroll(function() {
-		if ($window.scrollTop() > offset.top) {
+		if ($window.scrollTop() > offset.top && !isMobile()) {
 			$sidebar.stop().animate({
 				marginTop : $window.scrollTop() - offset.top + topPadding
 			});
@@ -68,3 +41,41 @@ $(function() {
 		}
 	});
 });
+
+function menuLinks(open) {
+	if (open) {
+		$(".menuLink").first().show(900, function showNext() {
+			$(this).next(".menuLink").show("fast", showNext);
+		});
+		$(".menuhr")
+				.each(
+						function(index) {
+							$(this)
+									.css(
+											"width",
+											((100 / $(".menuhr").length) * ($(".menuhr").length - index))
+													+ "%");
+						});
+	} else {
+		$(".menuLink").first().hide(200, function showNext() {
+			$(this).next(".menuLink").hide("fast", showNext);
+		});
+
+	}
+}
+
+function scrollFunction() {
+
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById('bgVideo').pause();
+	} else {
+		document.getElementById('bgVideo').play();
+	}
+}
+
+// Scrolling sidebar in blog
+
+function isMobile() {
+	return /Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i
+			.test(navigator.userAgent);
+}
